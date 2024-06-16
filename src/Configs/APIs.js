609bacbfb0eb411c77d82e3f5696264a501f6cdb
BEOBUJ <trackmix115@gmail.com>
2024-06-17 02:38:22 +0700

    add wiew register

--- src/Configs/APIs.js
+++ src/Configs/APIs.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 import cookie from "react-cookies";
 
-const BASE_URL = 'http://localhost:8000/QuanLyDeCuong/';
+const BASE_URL = 'http://localhost:8080/QuanLyDeCuong/';
 
 export const endpoints = {
     'subjects': '/api/subjects',
     'login': '/api/login/',
+    'register': '/api/users/', // Endpoint mới cho đăng ký
     'current-user': '/api/current-user/',
 
 }
